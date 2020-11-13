27f0a95441755cb93723babd0eeb4975c848e2e2
titikkoma88 <jie.koma88@gmail.com>
2020-11-13 17:13:47 +0700

    Initial commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import modulName from 'element/Button'
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
