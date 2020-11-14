f0d89bb79d17984b37266df8ab9c2ebd0dc5847c
titikkoma88 <jie.koma88@gmail.com>
2020-11-14 18:41:20 +0700

    component brand icon

---
+++ src/parts/IconText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+
+export default function IconText() {
+    return (
+        <button className="brand-text-icon" href="" type="link">
+            Atap<span className="text-gray-900">katulistiwa</span>
+        </button>
+    )
+}
