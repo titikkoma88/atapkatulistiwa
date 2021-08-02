ae62ef8019a2d7c3575a69f4a7cf3fc4b03bf952
titikkoma88 <jie.koma88@gmail.com>
2021-08-02 18:53:40 +0700

    Integrasi Landing Page and Tracing Error

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,45 +1,57 @@
-import React from 'react'
-import Fade from 'react-reveal/Fade';
-import Button from 'elements/Button'
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+import Button from "elements/Button";
 
 export default function MostPicked(props) {
-    return (
-        <section className="container" ref={props.refMostPicked}>
-            <Fade bottom>
-            <h4 className="mb-3">Most Picked</h4>
-            <div className="container-grid">
-                {props.data.map( (item, index) => {
-                    return (
-                        <div
-                            key={`mostpicked-${index}`} 
-                            className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
-                        >
-                            <Fade bottom delay={500 * index}>
-                            <div className="card card-featured">
-                                <div className="tag">
-                                    <span className="font-weight-light"> {item.status}</span>
-                                </div>
-                                <figure className="img-wrapper">
-                                    <img 
-                                        src={item.imageUrl} 
-                                        alt={item.name} 
-                                        className="img-cover" />
-                                </figure>
-                                <div className="meta-wrapper">
-                                    <Button type="link" className="stretched-link d-block text-white" href={`/properties/${item._id}`}>
-                                        <h5>{item.name}</h5>
-                                    </Button>
-                                    <span>
-                                        {item.city}, {item.country}
-                                    </span>
-                                </div>
-                            </div>
-                            </Fade>
+  return (
+    <section className="container" ref={props.refMostPicked}>
+      <Fade bottom>
+        <h4 className="mb-3">Most Picked</h4>
+        <div className="container-grid">
+          {props.data.map((item, index) => {
+            return (
+              <div
+                key={`mostpicked-${index}`}
+                className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+              >
+                <Fade bottom delay={500 * index}>
+                  <div className="card card-featured">
+                    {item.isClose && (
+                        <div className="tag">
+                          <span className="font-weight-light">Closed</span>
                         </div>
-                    );
-                })}
-            </div>
-            </Fade>
-        </section>
-    )
+                      )}
+                    <figure className="img-wrapper">
+                      <img
+                        src={
+                            item.imageId[0]
+                              ? `${process.env.REACT_APP_HOST}/${item.imageId[0].imageUrl}`
+                              : ""
+                          }
+                        alt={item.title}
+                        className="img-cover"
+                      />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <Button
+                        type="link"
+                        className="stretched-link d-block text-white"
+                        href={`/properties/${item._id}`}
+                      >
+                        <h5>{item.title}</h5>
+                      </Button>
+                      <span>
+                        {item.city}, {item.country}
+                      </span>
+                    </div>
+                  </div>
+                </Fade>
+              </div>
+            );
+          })}
+        </div>
+      </Fade>
+    </section>
+  );
 }
